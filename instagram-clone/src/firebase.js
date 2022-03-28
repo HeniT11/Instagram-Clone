418ff9c42db05a5a99885797b17ff94b756e0737
Henok Tesfahun <heni.me.tes@gmail.com>
2022-03-28 08:20:45 +0300

    Add Image Upload

--- instagram-clone/src/firebase.js
+++ instagram-clone/src/firebase.js
@@ -1,70 +1,9 @@
-/*import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-import 'firebase/compat/storage';
-
-const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyCBZfGQtWsUkZQDSE3qeo3-ct4yp8q2ZKc",
-    authDomain: "instagram-clone-81ece.firebaseapp.com",
-    projectId: "instagram-clone-81ece",
-    storageBucket: "instagram-clone-81ece.appspot.com",
-    messagingSenderId: "569074331652",
-    appId: "1:569074331652:web:e074e9d31875cdff2e0551",
-    measurementId: "G-TWR3PZRC7H"
-});
-
-const db = firebaseApp.firestore();
-const auth = firebase.auth();
-const storage = firebase.storage();
-
-export { db, auth, storage};
-*/
-/*import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-
-// TODO: Replace the following with your app's Firebase project configuration
-// See: https://firebase.google.com/docs/web/learn-more#config-object
-const firebaseConfig = {
-    apiKey: "AIzaSyCBZfGQtWsUkZQDSE3qeo3-ct4yp8q2ZKc",
-    authDomain: "instagram-clone-81ece.firebaseapp.com",
-    projectId: "instagram-clone-81ece",
-    storageBucket: "instagram-clone-81ece.appspot.com",
-    messagingSenderId: "569074331652",
-    appId: "1:569074331652:web:e074e9d31875cdff2e0551",
-    measurementId: "G-TWR3PZRC7H"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-
-// Initialize Cloud Firestore and get a reference to the service
-const db = getFirestore(app);
- 
-export { db };
-
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
- apiKey: "AIzaSyCBZfGQtWsUkZQDSE3qeo3-ct4yp8q2ZKc",
-    authDomain: "instagram-clone-81ece.firebaseapp.com",
-    projectId: "instagram-clone-81ece",
-    storageBucket: "instagram-clone-81ece.appspot.com",
-    messagingSenderId: "569074331652",
-    appId: "1:569074331652:web:e074e9d31875cdff2e0551",
-    measurementId: "G-TWR3PZRC7H"
-};
-
-const app = initializeApp(firebaseConfig);
-
-export const db = getFirestore(app);
-*/
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -83,4 +22,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-export const db = getFirestore(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+export { db, auth, storage};
